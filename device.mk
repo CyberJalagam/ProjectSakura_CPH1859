cf0180ca310e1c727cc5b8d1ebcce5492696396b
SamarV-121 <samarv121@pixelexperience.org>
2020-07-24 21:24:17 +0530

    CPH1859: Fix missing symbols in libshowlogo

--- device.mk
+++ device.mk
@@ -105,5 +105,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_showlogo
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oppo/CPH1859/CPH1859-vendor.mk)

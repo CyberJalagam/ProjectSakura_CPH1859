0ed0f88a61807aba9e812c0a52c25d5b940aa00a
Ayush <ayush@disroot.org>
2020-08-18 20:38:20 +0530

    CPH1859: Clean up props
    Signed-off-by: Ayush <ayush@disroot.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,9 +98,6 @@ TARGET_USES_MKE2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 AB_OTA_UPDATER := false
 
-# Vendor
-TARGET_COPY_OUT_VENDOR := vendor
-
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
@@ -139,3 +136,10 @@ TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
 # Vndk
 BOARD_VNDK_VERSION := current
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Will be used in future
+#TARGET_COPY_OUT_VENDOR := vendor
+#PRODUCT_VENDOR_MOVE_ENABLED := true
+#BOARD_VNDK_RUNTIME_DISABLE := true
+#PRODUCT_FULL_TREBLE_OVERRIDE := true
--- device.mk
+++ device.mk
@@ -129,10 +129,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
-# Product properties
--include $(LOCAL_PATH)/product_prop.mk
-
-
 # Manifest
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest.xml

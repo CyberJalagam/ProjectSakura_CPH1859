73d22aa9f57312ffd841b36d45f2fd06e90d4ab0
LuK1337 <priv.luk@gmail.com>
2020-11-13 10:55:57 +0530

    CPH1859: Build libhidltransport and libhwbinder

--- device.mk
+++ device.mk
@@ -100,7 +100,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhwbinder
 
 #WiFi
 PRODUCT_PACKAGES += \

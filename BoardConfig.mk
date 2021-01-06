738007e64432511fe0873e1980b25fdea0e8a5c5
Ayush <ayush@disroot.org>
2021-01-06 19:27:47 +0530

    CPH1859: Use separate flags for ramdisk and tags offset
    * and remove deprecated flags from mkbootimg args
    
    needs this patch to work: https://github.com/DerpFest-11/build/commit/efc92185b04d41c14ebad8748df7dd753d43dbf2
    
    Signed-off-by: Ayush <ayush@disroot.org>
    Signed-off-by: CyberJalagam <rbinternationalnetwork@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,9 @@ BUILD_BROKEN_DUP_RULES := true
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.configfs=true androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x14f88000 --second_offset 0x00e88000 --tags_offset 0x13f88000
+BOARD_KERNEL_TAGS_OFFSET := 0x13f88000
+BOARD_RAMDISK_OFFSET := 0x14f88000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 #TARGET_KERNEL_SOURCE := kernel/oppo/mt6771
